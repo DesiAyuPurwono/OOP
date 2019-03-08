f42a0a788b50530ee02b9326a73acd124dca9555
DesiAyuPurwono <48266222+DesiAyuPurwono@users.noreply.github.com>
2019-03-08 13:37:42 +0700

    Create desi_ayu_purwono_17090051_4c.java

---
+++ desi_ayu_purwono_17090051_4c.java
@@ -0,0 +1,7 @@
+package tugas;
+
+public class datang {
+    public static void main(string[] args){
+      system.out.println("Selamat Datang Di Object Oriented Programming \n Desi_Ayu_Purwono_17090051_4C");
+    }
+}
